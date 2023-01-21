07113278fd3a47a1de0781984b5bec071f4bf017
DjhonyMaga <djonatandeoliveira@hotmail.com>
2023-01-21 20:13:46 -0300

    projeto-final

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-    run: ['01-01', '01-02', '01-03', '01-06'],
-    water: ['01-04', '01-05', '01-07'],
-    food: ['01-01', '01-03', '01-07']
+
+button.addEventListener('click', add)
+form.addEventListener("change", save)
+
+function add() {
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+    const dayExist = nlwSetup.dayExists(today)
+
+    if(dayExist){
+        alert("Dia já incluso")
+        return
+    }
+
+    nlwSetup.addDay(today)
 }
+
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 
 nlwSetup.setData(data)
 nlwSetup.load()
